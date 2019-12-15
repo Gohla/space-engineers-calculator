95500ded7b6f9e7020cd3074af74fffc5f80782f
Gabriel Konat <gabrielkonat@gmail.com>
2019-12-15 10:11:57 +0100

    Generate input options handling with macro.

--- code/gui_iced/src/app.rs
+++ code/gui_iced/src/app.rs
@@ -1,36 +1,80 @@
 use std::fmt::Debug;
-use std::sync::Mutex;
 
 use iced::{Application, Color, Command, Element, Row, scrollable, Scrollable, Text};
 
-use lazy_static::lazy_static;
 use secalc_core::grid::GridCalculator;
 
 use crate::data_bind::{DataBind, DataBindMessage};
 
-lazy_static! {
-  static ref CALC: Mutex<GridCalculator> = Mutex::new(GridCalculator::default());
+macro_rules! create_input_options {
+  ($($field:ident, $type:ty, $message:ident, $name:expr, $format:expr);*) => {
+    struct InputOptions {
+      $($field: DataBind<$type>,)*
+    }
+
+    impl InputOptions {
+      fn new(calc: &GridCalculator) -> Self {
+        Self {
+          $($field: DataBind::new($name, calc.$field, format!($format, calc.$field)),)*
+        }
+      }
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum InputOptionMessage {
+      $($message(DataBindMessage),)*
+    }
+
+    impl InputOptions {
+      fn update(&mut self, message: InputOptionMessage, calc: &mut GridCalculator) {
+        match message {
+          $(InputOptionMessage::$message(m) => self.$field.update(m, &mut calc.$field),)*
+        }
+      }
+
+      pub fn view<M: 'static + Clone + Debug>(&mut self, on_change: impl 'static + Copy + Fn(InputOptionMessage) -> M) -> impl IntoIterator<Item=Element<M>> {
+        vec![
+          $(self.$field.view(move |s| on_change(InputOptionMessage::$message(s))),)*
+        ]
+      }
+    }
+  }
 }
+
+create_input_options!(
+  gravity_multiplier, f64, GravityMultiplier, "Gravity Multiplier", "{:.1}";
+  container_multiplier, f64, ContainerMultiplier, "Container Multiplier", "{:.1}";
+  planetary_influence, f64, PlanetaryInfluence, "Planetary Influence", "{:.1}";
+  additional_mass, f64, AdditionalMass, "Additional Mass", "{:.1}";
+  ice_only_fill, f64, IceOnlyFill, "Ice-only-fill", "{:.1}";
+  ore_only_fill, f64, OreOnlyFill, "Ore-only-fill", "{:.1}";
+  any_fill_with_ice, f64, AnyFillWithIce, "Any-fill with Ice", "{:.1}";
+  any_fill_with_ore, f64, AnyFillWithOre, "Any-fill with Ore", "{:.1}";
+  any_fill_with_steel_plates, f64, AnyFillWithSteelPlates, "Any-fill with Steel Plates", "{:.1}"
+);
 
 pub struct App {
   input_scrollable: scrollable::State,
   result_scrollable: scrollable::State,
-  gravity_multiplier: DataBind<f64>,
+  input_options: InputOptions,
+  calc: GridCalculator,
 }
 
 impl Default for App {
   fn default() -> Self {
+    let calc = GridCalculator::default();
     Self {
       input_scrollable: scrollable::State::default(),
       result_scrollable: scrollable::State::default(),
-      gravity_multiplier: DataBind::new("Gravity Multiplier", 1.0, "1.0", Box::new(|v| CALC.lock().unwrap().gravity_multiplier = v)),
+      input_options: InputOptions::new(&calc),
+      calc,
     }
   }
 }
 
 #[derive(Clone, Debug)]
 pub enum Message {
-  GravityMultiplier(DataBindMessage)
+  InputOptionChange(InputOptionMessage),
 }
 
 impl Application for App {
@@ -44,37 +88,50 @@ impl Application for App {
     String::from("Space Engineers Calculator")
   }
 
+
   fn update(&mut self, message: Message) -> Command<Message> {
     match message {
-      Message::GravityMultiplier(m) => self.gravity_multiplier.update(m),
+      Message::InputOptionChange(m) => self.input_options.update(m, &mut self.calc),
     }
     Command::none()
   }
 
   fn view(&mut self) -> Element<Message> {
-    let input: Element<_> = Scrollable::new(&mut self.input_scrollable)
-      .push(Text::new("Options"))
-      .push(self.gravity_multiplier.view(Message::GravityMultiplier))
-      .push(Text::new("Storage"))
-      .push(Text::new("Thrusters"))
-      .push(Text::new("Power"))
-      .push(Text::new("Hydrogen"))
+    let heading_size = 28;
+    let input = Scrollable::new(&mut self.input_scrollable)
+      .spacing(2)
+      .padding(2);
+
+    // Options
+    let mut input = input
+      .push(Text::new("Options").size(heading_size));
+    for elem in self.input_options.view(Message::InputOptionChange) {
+      input = input.push(elem);
+    }
+
+    let input: Element<_> = input
+      .push(Text::new("Storage").size(heading_size))
+      .push(Text::new("Thrusters").size(heading_size))
+      .push(Text::new("Power").size(heading_size))
+      .push(Text::new("Hydrogen").size(heading_size))
       .into();
     let result: Element<_> = Scrollable::new(&mut self.result_scrollable)
-      .push(Text::new("Mass"))
-      .push(Text::new("Volume"))
-      .push(Text::new("Items"))
-      .push(Text::new("Force & Acceleration"))
-      .push(Text::new("Power"))
-      .push(Text::new("Hydrogen"))
+      .spacing(2)
+      .padding(2)
+      .push(Text::new("Mass").size(heading_size))
+      .push(Text::new("Volume").size(heading_size))
+      .push(Text::new("Items").size(heading_size))
+      .push(Text::new("Force & Acceleration").size(heading_size))
+      .push(Text::new("Power").size(heading_size))
+      .push(Text::new("Hydrogen").size(heading_size))
       .into();
 
     let content: Element<_> = Row::new()
-      .spacing(20)
-      .padding(20)
+      .spacing(10)
+      .padding(10)
       .push(input)
       .push(result)
       .into();
-    content.explain(Color::BLACK)
+    content.explain(Color::from_rgb(1.0, 0.0, 0.0))
   }
 }
--- code/gui_iced/src/data_bind.rs
+++ code/gui_iced/src/data_bind.rs
@@ -1,13 +1,12 @@
 use std::fmt::Debug;
 use std::str::FromStr;
 
-use iced::{Color, Element, Length, Row, Text, text_input, TextInput};
+use iced::{Color, Element, Length, Row, Text, text_input, TextInput, VerticalAlignment};
 
 pub struct DataBind<T> {
   label: String,
   default: T,
   placeholder: String,
-  update: Box<dyn Fn(T) -> ()>,
   value: String,
   error: bool,
   state: text_input::State,
@@ -17,43 +16,44 @@ pub struct DataBind<T> {
 pub struct DataBindMessage(String);
 
 impl<T: Copy + FromStr> DataBind<T> {
-  pub fn new<L: Into<String>, P: Into<String>>(label: L, default: T, placeholder: P, update: Box<dyn Fn(T) -> ()>) -> Self {
+  pub fn new<L: Into<String>, P: Into<String>>(label: L, default: T, placeholder: P) -> Self {
     let label = label.into();
     let placeholder = placeholder.into();
     let value = String::new();
     let error = false;
     let state = text_input::State::default();
-    Self { label, default, placeholder, update, value, error, state }
+    Self { label, default, placeholder, value, error, state }
   }
 
-  pub fn update(&mut self, message: DataBindMessage) {
-    if message.0.is_empty() {
-      (self.update)(self.default);
-      self.error = false;
-    } else {
-      if let Ok(v) = T::from_str(&message.0) {
-        (self.update)(v);
-        self.error = false;
-      } else {
-        (self.update)(self.default);
-        self.error = true;
-      }
-    }
-    self.value = message.0;
+  pub fn update(&mut self, message: DataBindMessage, val: &mut T) {
+    let text = message.0;
+    let (v, error) = match (text.is_empty(), T::from_str(&text)) {
+      (true, _) => (self.default, false),
+      (_, Err(_)) => (self.default, true),
+      (false, Ok(v)) => (v, false)
+    };
+    *val = v;
+    self.error = error;
+    self.value = text;
   }
 
   pub fn view<M: 'static + Clone + Debug>(&mut self, on_change: impl 'static + Fn(DataBindMessage) -> M) -> Element<M> {
-    let text = Text::new(&self.label);
+    let text = Text::new(&self.label)
+      .width(Length::Shrink)
+      .vertical_alignment(VerticalAlignment::Center);
     let text = if self.error {
       text.color(Color::from_rgb(0.8, 0.0, 0.0))
     } else {
       text
     };
     Row::new()
+      .spacing(4)
+      .padding(4)
       .push(text)
       .push(
         TextInput::new(&mut self.state, &self.placeholder, &self.value, move |s| on_change(DataBindMessage(s)))
-          .width(Length::Units(100))
+          .width(Length::Units(50))
+          .padding(1)
       )
       .into()
   }
